3c07e2ca2cd50bdba60fd14723b817fc7e2cd86a
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-02-04 12:39:39 +0300

    11_commit_rt

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
 import React from 'react';
 
-// function App() {
-// 	return (
-//   <>
-//     <ul>
-//       <li>text1</li>
-//       <li>text2</li>
-//       <li>text3</li>
-//     </ul>
-//     <ul>
-//       <li>text1</li>
-//       <li>text2</li>
-//       <li>text3</li>
-//     </ul>;
-//   </>
-//   )
-// }
-
-function App_2() {
-	return (
-  <ul>
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>;
-  </ul>
-  )
+function App() {
+	return <input />;
 }
-export default App_2;
+export default App;
