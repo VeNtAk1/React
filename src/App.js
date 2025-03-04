492fd53f0cc8568bd1c4d71de6715aa72cf6aa61
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-04 09:02:30 +0300

    13-15_React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 function App() {
-	const str1 = 'text1';
-	const str2 = 'text2';
+	const name = 'user';
+	const age  = '30';
+	const arr = [1, 2, 3, 4, 5];
 
-
-	return <div>
-		<p> { str1 } </p>
-	</div>;
+	return <>
+	<div>
+		name: { name } <br></br>
+		age: { age }
+	</div>,
+	<ul>
+		<li>{arr[0]}</li>
+		<li>{arr[1]}</li>
+		<li>{arr[2]}</li>
+		<li>{arr[3]}</li>
+		<li>{arr[4]}</li>
+	</ul>
+	</>
 }
 export default App;
