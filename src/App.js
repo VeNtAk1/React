1c13d72a97626b63cb82c94e3d630df18d539644
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-04 09:25:29 +0300

    24_React

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from 'react';
 
 function App() {
-	const str = <main>
-		text
-	</main>;
+	const num1 = 3;
+	const num2 = 2;
+	const name = 'john';
+	const surname = 'smit';
+	const num = 4;
+
 
 	return <>
-	{ str }
-	<ul>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-	<div>
-		<tr>
-			1
-			2
-			3
-		</tr>
-		<tr>
-			1
-			2
-			3
-		</tr>
-		<tr>
-			1 
-			2 
-			3
-		</tr>
-	</div>
+		result: {num1 ** num2} <br></br>
+		result: {name + ' ' + surname} <br></br>
+		result: {Math.sqrt(num)}
 	</>
 }
 export default App;
