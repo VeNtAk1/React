c05f63287f3e7c674c6e9d8fb4135ec32f0b1828
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-04 09:23:01 +0300

    21-24_React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 
 function App() {
-	const li1 = <li>text1</li>;
-	const li2 = <li>text2</li>;
-	const li3 = <li>text3</li>;
-	const items = <><li>text1</li><li>text2</li><li>text3</li></>;
+	const str = <main>
+		text
+	</main>;
 
 	return <>
-	{li1}{li2} {li3}
-	{ items }
+	{ str }
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+	<div>
+		<tr>
+			1
+			2
+			3
+		</tr>
+		<tr>
+			1
+			2
+			3
+		</tr>
+		<tr>
+			1 
+			2 
+			3
+		</tr>
+	</div>
 	</>
 }
 export default App;
