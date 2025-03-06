36af9216d40121f2240aca32baafc308f9429c89
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-06 12:24:18 +0300

    40_React

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
 function App() {
-	const arr = ['a', 'b', 'c', 'd', 'e'];
-	const res = [];
-
-	for (const elem in arr){
-		res.push(<p>{arr[elem]}</p>)
-	}
-	return <>
-		<ul>
-			{res}
-		</ul>
-
-	</>;
+	const arr = [1, 2, 3, 4, 5];
+	
+	const res = arr.map(function(item, index) {
+		return <p key={index}>{item}</p>;
+	});
+	
+	return <div>
+		{res}
+	</div>;
 }
 export default App; 
