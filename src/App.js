be65ce97bb332f9442f8a99582431b8b16d5f692
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-06 12:35:07 +0300

    41-42_React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 function App() {
-	const arr = [1, 2, 3, 4, 5];
+	const users = [
+		{id: 1, name: 'user1', surn: 'surn1', age: 30},
+		{id: 1, name: 'user2', surn: 'surn2', age: 31},
+		{id: 1, name: 'user3', surn: 'surn3', age: 32},
+	];
+	const res = users.map(function(item, index){
+		return<li key={item.id}>{item.name}, {item.surn}, {item.age}</li>
+	})	
 	
-	const res = arr.map(function(item, index) {
-		return <p key={index}>{item}</p>;
-	});
-	
-	return <div>
-		{res}
-	</div>;
+	return <>
+		<ul>
+			{res}
+		</ul>
+	</>;
 }
 export default App; 
