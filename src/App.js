42d0e185db959313d69bd9347dae0c3734f5477c
VeNtAk1 <143203765+VeNtAk1@users.noreply.github.com>
2025-03-06 12:02:16 +0300

    39_React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 
 function App() {
-	const arr = [
-		<li>1</li>,
-		<li>2</li>,
-		<li>3</li>,
-		<li>4</li>,
-		<li>5</li>,
-		];
+	const arr = ['a', 'b', 'c', 'd', 'e'];
+	const res = [];
 
-	const arr2 = [];
-
-	for (let i = 0; i <= 5; i++){
-		arr2.push(<p>{i}</p>)
+	for (const elem in arr){
+		res.push(<p>{arr[elem]}</p>)
 	}
 	return <>
 		<ul>
-			{ arr }
-		</ul>
-		
-		<ul>
-			{ arr2 }
+			{res}
 		</ul>
 
 	</>;
